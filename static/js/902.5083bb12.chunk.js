12850dc70934d88ba8528e609ad332a26c18dde3
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2022-12-22 09:19:33 +0000

    Deploying to gh-pages from @ yuriiavr/goit-react-hw-05-movies@7f7f86ce9b1173c786b6ae2bf51aa8049de149ea 🚀

---
+++ static/js/902.5083bb12.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[902],{1902:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("h1",{children:"C\u0442\u043e\u0440\u0456\u043d\u043a\u0430 \u043d\u0435 \u0437\u043d\u0430\u0439\u0434\u0435\u043d\u0430 :("})}}}]);
+//# sourceMappingURL=902.5083bb12.chunk.js.map
