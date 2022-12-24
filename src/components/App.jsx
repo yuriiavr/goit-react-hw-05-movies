bdb0e6e5e8611066cf70081a7106a41b1b87f05a
yuriiavr <yurii.avramets@gmail.com>
2022-12-24 13:26:00 +0300

    3213123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ const Reviews = lazy(() => import('../views/Reviews'));
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
+      <Route path="/goit-react-hw-05-movies/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
 
